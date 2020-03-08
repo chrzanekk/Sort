3669b6ec30133e81a705048d5e26b525562114dc
Konrad <chrzanekk@gmail.com>
2020-03-08 19:31:07 +0100

    Implemented Quick Sort, Bubble Sort and Insert Sort, added time durations

--- src/pl/com/konrad/sort/Main.java
+++ src/pl/com/konrad/sort/Main.java
@@ -6,22 +6,38 @@ public class Main {
         BubbleSort bubbleSort = new BubbleSort();
         InsertSort insertSort = new InsertSort();
         QuickSort quickSort = new QuickSort();
+        TimeDuration timeDuration = new TimeDuration();
 
         int[] unsortedList = {34, 13, 76, 1, 97, 61, 6, 93, 12, 32, 11, 88, 31, 84, 16, 19, 2, 66, 33, 82, 5, 43, 14, 101, 124, 4, 9, 20, 10, 99, 77};
         System.out.println("Unsorted list: ");
         printArray(unsortedList);
 
+        long startTimeBubbleSort = System.nanoTime();
         int[] bubbleSorted = bubbleSort.bubbleSort(unsortedList);
+        long endTimeBubbleSort = System.nanoTime();
         System.out.println("Sorted list by using \" bubble sort\": ");
         printArray(bubbleSorted);
 
+        long startTimeInsertSort = System.nanoTime();
         int[] insertSorted = insertSort.insertSort(unsortedList);
+        long endTimeInsertSort = System.nanoTime();
         System.out.println("Sorted list by using \" insert sort\": ");
         printArray(insertSorted);
 
+        long startTimeQuickSort = System.nanoTime();
         int[] quickSorted = quickSort.quickSort(unsortedList, 0,unsortedList.length-1);
+        long endTimeQuickSort = System.nanoTime();
         System.out.println("Sorted list by using \" quick sort\": ");
         printArray(quickSorted);
+
+        System.out.println("Sorting time durations: ");
+        System.out.println("1. Bubble sort: " + timeDuration.timeDuration(startTimeBubbleSort,endTimeBubbleSort) +
+                "ms.");
+        System.out.println("2. Insertion sort: " + timeDuration.timeDuration(startTimeInsertSort,endTimeInsertSort) + "ms" +
+                ".");
+        System.out.println("3. Quick sort: " + timeDuration.timeDuration(startTimeQuickSort,endTimeQuickSort) + "ms" +
+                ".");
+
     }
 
     public static void printArray(int[] arrayToPrint) {
---
+++ src/pl/com/konrad/sort/TimeDuration.java
@@ -0,0 +1,10 @@
+package pl.com.konrad.sort;
+
+public class TimeDuration {
+    public long timeDuration(long startTime, long endTime) {
+        long timeDuration = endTime - startTime;
+        return timeDuration;
+    }
+
+    }
+
